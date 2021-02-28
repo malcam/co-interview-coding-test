338d5313eb5d614c1b2bd0c0df017ce5e6bad5bc
malcam <ing.malcam@gmail.com>
2021-02-27 19:12:06 -0600

    Update unit test
    Add basic unit tests based on products_after_30_days.txt before making codebase
    modifications.
    
    Unit testing ensures that all code meets quality standards before it's
    deployed.

--- spec/coTestSpec.js
+++ spec/coTestSpec.js
@@ -1,15 +1,69 @@
-const expect = require('chai').expect;
+/* eslint-disable prefer-arrow-callback */
+const { expect } = require('chai');
 
 const coTest = require('../src/coTest');
-const CarInsurance = coTest.CarInsurance;
-const Product = coTest.Product;
 
-describe("Co Test", function() {
+const { CarInsurance } = coTest;
+const { Product } = coTest;
 
-  it("should foo", function() {
-    const coTest = new CarInsurance([ new Product("foo", 0, 0) ]);
-    const products = coTest.updatePrice();
-    expect(products[0].name).equal("fixme");
+describe('Co Test', function () {
+  context('when name is Medium Coverage', function () {
+    const data = require('./medium-coverage-data.json');
+
+    const { name } = data;
+    const initialSellIn = data.initialValues.sellIn;
+    const initialPrice = data.initialValues.price;
+
+    it('should return the same values', function () {
+      const carInsurance = new CarInsurance([new Product(name, initialSellIn, initialPrice)]);
+
+      data.days.forEach((element) => {
+        const products = carInsurance.updatePrice();
+
+        expect(products[0].name).to.equal(data.name);
+        expect(products[0].sellIn).to.equal(element.values.sellIn);
+        expect(products[0].price).to.equal(element.values.price);
+      });
+    });
   });
 
+  context('when name is Full Coverage', function () {
+    const data = require('./full-coverage-data.json');
+
+    const { name } = data;
+    const initialSellIn = data.initialValues.sellIn;
+    const initialPrice = data.initialValues.price;
+
+    it('should return the same values', function () {
+      const carInsurance = new CarInsurance([new Product(name, initialSellIn, initialPrice)]);
+
+      data.days.forEach((element) => {
+        const products = carInsurance.updatePrice();
+
+        expect(products[0].name).to.equal(data.name);
+        expect(products[0].sellIn).to.equal(element.values.sellIn);
+        expect(products[0].price).to.equal(element.values.price);
+      });
+    });
+  });
+
+  context('when name is Low Coverage', function () {
+    const data = require('./low-coverage-data.json');
+
+    const { name } = data;
+    const initialSellIn = data.initialValues.sellIn;
+    const initialPrice = data.initialValues.price;
+
+    it('should return the same values', function () {
+      const carInsurance = new CarInsurance([new Product(name, initialSellIn, initialPrice)]);
+
+      data.days.forEach((element) => {
+        const products = carInsurance.updatePrice();
+
+        expect(products[0].name).to.equal(data.name);
+        expect(products[0].sellIn).to.equal(element.values.sellIn);
+        expect(products[0].price).to.equal(element.values.price);
+      });
+    });
+  });
 });
